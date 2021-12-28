9ed7e82ee4c5e41486625e650911183541e09196
Saburov <the.saburchik@gmail.com>
2021-12-28 18:29:38 +0700

    checking animation on iOS

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import './App.css'
 // == Components:
 import Profile from './components/Profile/Profile'
 import Header from './components/Main/Header'
+import DrawImages from './components/DrawImages'
 import Home from './components/Articles/Home'
 import Resume from './components/Articles/Resume'
 import Portfolio from './components/Articles/Portfolio'
@@ -35,6 +36,9 @@ const App = () => {
               <section className="information">
                 <Header />
                 <main className="main">
+                  <div style={{ opacity: '0', position: 'absolute', top: '-5000px' }}>
+                    <DrawImages />
+                  </div>
                   <Switch>
                     <Route exact path="/" component={Home} />
                     <Route exact path="/resume" component={Resume} />
