ab1a39c18e519175d62568415cee781158175744
Saburov <the.saburchik@gmail.com>
2022-05-23 23:54:30 +0700

    update information

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,57 @@
 // == Base:
-import React, { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import PropagateLoader from 'react-spinners/PropagateLoader'
+import React, { useState, useEffect } from "react";
+import { HashRouter, Route, Switch } from "react-router-dom";
+import PropagateLoader from "react-spinners/PropagateLoader";
 // == Styles:
-import './App.css'
+import "./App.css";
 // == Components:
-import Profile from './components/Profile/Profile'
-import Header from './components/Main/Header'
-import DrawImages from './components/DrawImages'
-import Home from './components/Articles/Home'
-import Resume from './components/Articles/Resume'
-import Portfolio from './components/Articles/Portfolio'
+import Profile from "./components/Profile/Profile";
+import Header from "./components/Main/Header";
+import DrawImages from "./components/DrawImages";
+import Home from "./components/Articles/Home";
+import Resume from "./components/Articles/Resume";
+import Portfolio from "./components/Articles/Portfolio";
 
 const App = () => {
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    setLoading(true)
+    setLoading(true);
     setTimeout(() => {
-      setLoading(false)
-    }, 300)
-  }, [])
+      setLoading(false);
+    }, 300);
+  }, []);
 
   return (
-    <Router>
-      {
-        loading ?
-          <div className="loadingpage">
-            <PropagateLoader color={'#fff'} loading={loading} size={15} />
+    <HashRouter>
+      {loading ? (
+        <div className="loadingpage">
+          <PropagateLoader color={"#fff"} loading={loading} size={15} />
+        </div>
+      ) : (
+        <div className="wrapper">
+          <div className="container" id="tabs">
+            <Profile />
+            <section className="information">
+              <Header />
+              <main className="main">
+                <div
+                  style={{ opacity: "0", position: "absolute", top: "-5000px" }}
+                >
+                  <DrawImages />
+                </div>
+                <Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route exact path="/resume" component={Resume} />
+                  <Route exact path="/portfolio" component={Portfolio} />
+                </Switch>
+              </main>
+            </section>
           </div>
-          :
-          <div className="wrapper">
-            <div className="container" id="tabs">
-              <Profile />
-              <section className="information">
-                <Header />
-                <main className="main">
-                  <div style={{ opacity: '0', position: 'absolute', top: '-5000px' }}>
-                    <DrawImages />
-                  </div>
-                  <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route exact path="/resume" component={Resume} />
-                    <Route exact path="/portfolio" component={Portfolio} />
-                  </Switch>
-                </main>
-              </section>
-            </div>
-          </div>
-      }
-    </Router>
-  )
-}
+        </div>
+      )}
+    </HashRouter>
+  );
+};
 
-export default App
+export default App;
